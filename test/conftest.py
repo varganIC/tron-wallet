b9a118e5e6963ebb2c443aa29707f8bacd1c9068
Набоков Алексей Юрьевич <ayu_nabokov@controlsystems.ru>
2025-01-24 13:29:48 +0300

    pre-commit

--- test/conftest.py
+++ test/conftest.py
@@ -1,7 +1,4 @@
-
 import pytest
-from fastapi import FastAPI
-from fastapi.middleware import Middleware
 from fastapi.testclient import TestClient
 
 from api_routes.metadata import api_prefix
@@ -10,23 +7,11 @@ from app.main import app
 
 def pytest_configure():
     pytest.api_prefix = api_prefix
-    pytest.history_id = api_prefix
-
-
-def remove_middleware(app: FastAPI, target: str) -> FastAPI:
-    new_middlewares: list[Middleware] = []
-    for middleware in app.user_middleware:
-        if not middleware.cls.__name__ == target:
-            new_middlewares.append(middleware)
-    app.user_middleware = new_middlewares
-    app.middleware_stack = app.build_middleware_stack()
-    return app
 
 
 @pytest.fixture(scope="module")
 def test_app():
     client = TestClient(app)
-    remove_middleware(app, 'BaseHTTPMiddleware')
     yield client
 
 
